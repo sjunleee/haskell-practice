e3ebfd171e910b8ccf7a0752382f289c65dd5161
Seungjun Lee <seungjun.lee.22@ucl.ac.uk>
2023-04-04 17:30:13 +0900

    Simple functions

--- baby.hs
+++ baby.hs
@@ -1,11 +1,10 @@
 doubleMe x = x + x
+
 doubleUs x y = x * 2 + y * 2
 
 doubleFunc x y = doubleMe x + doubleUs y
 
-doubleSmallNum x = if x > 100 
-    then x
-    else x * 2
+doubleSmallNum x = if x > 100 then x else x * 2
 
 length' xs = sum[1|_<-xs]
 
