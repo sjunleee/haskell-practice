e8bf70f807a7ad4a075c4f615d8533d9b0f0adb7
Seungjun Lee <seungjun.lee.22@ucl.ac.uk>
2023-04-04 17:12:53 +0900

    simple functions

---
+++ baby.hs
@@ -0,0 +1,12 @@
+doubleMe x = x + x
+doubleUs x y = x * 2 + y * 2
+
+doubleFunc x y = doubleMe x + doubleUs y
+
+doubleSmallNum x = if x > 100 
+    then x
+    else x * 2
+
+length' xs = sum[1|_<-xs]
+
+keepLowerCase st = [ c | c <- st, c 'elem' ['a'..'z']]
